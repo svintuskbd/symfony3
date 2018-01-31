fd129daac6d9975eacd77bf1c23a845caa9de7a5
svintuskbd <swintuskbd@gmail.com>
2018-01-31 20:44:08 +0200

    fix

--- src/ScriptBundle/Form/TaskType.php
+++ src/ScriptBundle/Form/TaskType.php
@@ -1,11 +1,12 @@
 <?php
 namespace ScriptBundle\Form;
 
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class TaskType
+class TaskType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
